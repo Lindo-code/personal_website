22ef6bc025cb64430b57140c722ee8a2d931eb68
Lindo-code <sdrowvieli1@gmail.com>
2022-03-12 07:43:48 +0200

    renamed myScript.js to main.js and added js file for each page.

---
+++ javaScript/home.js
@@ -0,0 +1,13 @@
+function randomStr() {
+  // get greeting
+  const str = document.getElementById("greeting");
+  const arr = ["HEY! LET'S TALK :)",
+    "HI! LET'S GET INTOUCH :)"]
+  const num = getRandomNum(0, arr.length - 1);
+  str.innerText = arr[num];
+}
+function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
--- javaScript/main.js
+++ javaScript/main.js
@@ -1,10 +1,14 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
-  scrollFunction()};
+  scrollFunction();
+};
 
+window.onload = function() {
+  randomStr();
+};
 function scrollFunction() {
+  //Get the button:
+  const mybutton = document.getElementById("myBtn");
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
