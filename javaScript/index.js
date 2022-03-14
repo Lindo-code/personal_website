a4f1e709114a83a2a2ef9f451cb432482712c3b5
Lindo-code <sdrowvieli1@gmail.com>
2022-03-14 09:17:08 +0200

    changed email icon color to white and re-added randomstr function to index.js file

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,3 +1,15 @@
+function randomStr() {
+  // get greeting
+  const str = document.getElementById("greeting");
+  const arr = ["🔶 HELLO & WELCOME TO MY WEBSITE :)"]
+  const num = getRandomNum(0, arr.length - 1);
+  str.innerText = arr[num];
+}
+function getRandomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function delay(url) {
   setTimeout(function() {
     window.location = url;
