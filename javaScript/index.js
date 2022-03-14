fb64f22c8b27ef7b74454fb316d0c2f11610af7d
Lindo-code <sdrowvieli1@gmail.com>
2022-03-14 08:53:07 +0200

    removed unused functions in index.js file

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,16 +1,3 @@
-function randomStr() {
-  // get greeting
-  const str = document.getElementById("greeting");
-  const arr = ["HEY! LET'S TALK :)",
-    "HI! LET'S GET INTOUCH :)"]
-  const num = getRandomNum(0, arr.length - 1);
-  str.innerText = arr[num];
-}
-function getRandomNum(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 function delay(url) {
   setTimeout(function() {
     window.location = url;
